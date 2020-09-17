114ad70ae249d0e13734cfa4883038e16a40dc9e
98changl <98changl@gmail.com>
2020-09-16 20:01:11 -0400

    Update GroceryItem.java

--- project1/GroceryItem.java
+++ project1/GroceryItem.java
@@ -1,5 +1,18 @@
-package project1;
+/**
+ * This class defines the abstract data type GroceryItem
+ * @author Liman Chang, Kenneth Christian
+ */
 
 public class GroceryItem {
-
+	private String name;
+	private double price;
+	private boolean taxable;
+	
+	public boolean equals(Object obj) {
+		return false;
+	}
+	
+	public String toString() {
+		return name;
+	}
 }

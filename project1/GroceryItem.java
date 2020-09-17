86993568e6548fca8ab8570247b4975765602f2c
98changl <98changl@gmail.com>
2020-09-17 19:09:43 -0400

    Update GroceryItem.java

--- project1/GroceryItem.java
+++ project1/GroceryItem.java
@@ -8,12 +8,44 @@ public class GroceryItem {
 	private double price;
 	private boolean taxable;
 	
+	/**
+	 * Check to see if two strings are equal
+	 * 
+	 * @param first string to compare
+	 * @param seccond string to compare
+	 * @return true if equal, false if not equal
+	 */
+	private boolean stringEquals(String a, String b) {
+		if (a.length() != b.length()) {
+			return false;
+		}
+		
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) != b.charAt(i)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	public boolean equals(Object obj) {
 		boolean isEqual = true;
+		
 		if (obj instanceof GroceryItem) {
 			GroceryItem item = (GroceryItem) obj;
 			
+			if (stringEquals(name, item.name) == false) {
+				isEqual = false;
+			}
 			
+			if (price != item.price) {
+				isEqual = false;
+			}
+			
+			if (taxable != item.taxable) {
+				isEqual = false;
+			}
 		} else {
 			isEqual = false;
 		}
@@ -21,6 +53,7 @@ public class GroceryItem {
 	}
 	
 	public String toString() {
+		
 		return name;
 	}
 }

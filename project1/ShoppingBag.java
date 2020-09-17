bd739c7370131c6019cc20d745ab80dc9052e47e
98changl <98changl@gmail.com>
2020-09-16 20:00:30 -0400

    Update ShoppingBag.java

--- project1/ShoppingBag.java
+++ project1/ShoppingBag.java
@@ -1,7 +1,53 @@
-package project1;
+/**
+ * The container class that defines the abstract data type
+ * @author Liman Chang, Kenneth Christian
+ */
 
 public class ShoppingBag {
-
+	private GroceryItem[] bag;	// array-based implementation of the bag
+	private int size;			// number of items currently in the bag
+	
+	public ShoppingBag() {
+		
+	}
+	
+	/**
+	 * helper method to find an item
+	 * 
+	 * @param grocery item to be removed
+	 * @return index of grocery item, -1 if item doesn't exist
+	 */
+	private int find(GroceryItem item) {
+		return 0;
+	}
+	
+	/**
+	 * helper method to grow the capacity
+	 */
+	private void grow() {
+		
+	}
+	
+	public void add(GroceryItem item) {
+		
+	}
+	
+	public boolean remove(GroceryItem item) {
+		return false;
+	}
+	
+	public double salesPrice() {
+		return 0.0;
+	}
+	
+	public double salesTax() {
+		return 0.0;
+	}
+	
+	public void print() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

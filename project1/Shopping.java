aec647f267a11dfc1b486b37fe560d0d74a36fe2
kennethchris99 <71343141+kennethchris99@users.noreply.github.com>
2020-09-20 12:48:55 -0400

    Update Shopping.java

--- project1/Shopping.java
+++ project1/Shopping.java
@@ -5,52 +5,69 @@
 import java.util.*;;
 public class Shopping {
 
-	public Shopping() {
-		// TODO Auto-generated constructor stub
-	}
+	public class Shopping {//////////////////////////////// for now use "," to seperate. itemname,price,true/false////////////////////
+
+    //create only one instance of the shoppingBag
+    private static ShoppingBag instance = new ShoppingBag();
+
 	
 	//main method from driver class will call this for I/O. 
 	public void run() {
 		
 		Scanner sc = new Scanner(System.in); //to get input
-		
 		String input = ""; //will get input C, A, R, or D
 	   
-		
 	    
 	    //this while loop will continue asking the user for new commands after each
 	    //new item is added to the array until they decide to checkout
 	    
 		while(!input.equals("C")) {//C for Checkout  
 		System.out.println("\nPlease enter one of the following Commands: C, A, R, or D");
-		
 		input = sc.next();
-			
-		if(input.equals("A")) {//add to the bag
-			
-		//for debugging//		
-		System.out.println("Please enter");  
+		
 		String item = "";
+		
+		//Add to bag
+		if(input.equals("A")) {
+				
+			
+		System.out.println("Please enter");  
 		item = sc.next();
-		System.out.println("you added: " + item);
-		//for debugging//
+		
+           
+              GroceryItem it = new GroceryItem(item);//create new item
+              
+              instance.add(it);//add the new item into the bag
+        	 	
 		
 		}
 		
-		else if(input.equals("R")) {//remove from the bag
+		//remove from bag
+		else if(input.equals("R")) {
 			 
 		}
-		else if(input.equals("D")) {//display
+		
+		//display bag
+		else if(input.equals("D")) {
+		
+               
+               instance.print();
+                    
 			
 		}
+		
+		//checkout
 		else if(input.equals("C")) {
 			System.out.println("thank You, have a nice day");
 		}
+		
+		//if user inputs in valid option
 		else {
 			System.out.println("invalid input");
 		}
 		
-	}//end of while loop
+	}
 	
 	}
 }
+

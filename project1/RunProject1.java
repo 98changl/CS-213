82836872d565131f94c600c84d61ac5500ff3f35
98changl <98changl@gmail.com>
2020-09-24 17:07:25 -0400

    Update RunProject1.java

--- project1/RunProject1.java
+++ project1/RunProject1.java
@@ -1,8 +1,9 @@
 /**
  * This is a driver class to run project 1
+ * The main method calls the run() method in the shopping class
+ * 
  * @author Kenneth Christian, Liman Chang
  */
-
 public class RunProject1 {
 	public static void main(String[] args) {
 		new Shopping().run();

add56ee05a66e3bdd1c64dfa55e31601afe851f6
98changl <98changl@gmail.com>
2020-09-24 15:53:05 -0400

    Update ShoppingBag.java

--- project1/ShoppingBag.java
+++ project1/ShoppingBag.java
@@ -1,87 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The container class that defines the abstract data type
+ * @author Liman Chang, Kenneth Christian
  */
 
-
 public class ShoppingBag {
-
     private GroceryItem[] bag;	// array-based implementation of the bag
     private int size;			// number of items currently in the bag
 
+    /**
+     * Initializes the shopping bag with an initial array size of 2
+     */
     public ShoppingBag() {
-
-        bag = new GroceryItem[5];//initial size of array = 5
-
-    }
-
-    //how many items in array
-    public int getSize() {
-        size = 0;
-        for (int x = 0; x < bag.length; x++) {
-            if (bag[x] != null) {
-                size++;
-            }
-
-        }
-        return size;
-    }
-
-    //helper methdo to grow array by 5
-    private void grow() {
-        /*
-                        All the index are filled.
-                        This creates a new array (newBag) that has size 5 greater than bag (initial array),
-                        copies the content of bag into it and assigns that array to bag (the original array)
-         */
-
-        GroceryItem[] newBag = new GroceryItem[bag.length + 5];
-        System.arraycopy(bag, 0, newBag, 0, bag.length);
-
-        bag = newBag;
-        size = bag.length;
-    }
-
-    //empty the bag IF checkout
-    public void setEmpty() {
-        bag = new GroceryItem[5];
+        bag = new GroceryItem[2];
     }
 
     /**
-     * @param item 
      * this method adds item to the array bag. if the array is full
-     * then will call helper grow method to crease the size by 5 and then add
+     * then will call helper grow method to increase the size by 5 and then add
      * the item to the next empty index. Assume if the array is partially empty
      * then it will be the last index that is empty.
+     * 
+     * @param GroceryItem to be added to Shopping Bag
      */
     public void add(GroceryItem item) {
-
-        GroceryItem refrence = item;
-
-        for (int x = 0; x < bag.length; x++) {
-
-            if (bag[x] == null) {
-
-                bag[x] = item;
-
-                break; //break out of for loop once the next empty index has been filled
-
-            }
-        }
-
-        //if the array is full, increase and copy the old array then add to the next empty index
-        for (int t = 0; t < bag.length; t++) {
-
-            if (bag[t] != null && bag[bag.length - 1] != null) {
-
-                grow(); //calls the helper grow method to copy the array and grow and add the item to the next empty index
-
-                bag[t] = item;
-
-                break; //break out of for loop
-            }
-        }
+    	if (this.size == bag.length) { // the shopping bag is at maximum capacity
+    		grow();
+    	}
+    	
+    	bag[size] = item;
+    	this.size++;
     }
 
     /**
@@ -92,106 +39,106 @@ public class ShoppingBag {
      * @return return_value
      */
     public boolean remove(GroceryItem item) {
-
-        int find = find(item);
-
-        boolean return_value = false;
-
-        if (find != -1) {
-
-            for (int x = 0; x < bag.length; x++) {
-                if (bag[x] == null) {
-                    bag[find] = bag[x - 1];
-                    bag[x - 1] = null;
-
-                    break;
-                }
-            }
-
-            return_value = true; //item found and removed
-
-        } else if (find == -1) {
-            return_value = false; //item not found in bag
+        int index = find(item);
+        
+        if (index == -1) { // item was not found
+        	return false;
         }
-
-        return return_value;
-    }
-
-    public boolean isEmpty() {
-
-        boolean return_value = false; //assume bag is not empty
-
-        if (getSize() == 0) {
-            return_value = true;
+        
+        if (index != size) {
+        	bag[index] = bag[size-1];	// swap index and last item
         }
-
-        return return_value;
+        bag[size-1] = null;
+        this.size--;
+        
+        return true;
     }
 
     /**
-     * @returns price
+     * Calculates the sales price of items in the shopping bag
+     * 
+     * @returns total sales price
      */
     public double salesPrice() {
+        double salesPrice = 0;
 
-        double price = 0;
-
-        for (int x = 0; x < bag.length; x++) {
-            if (bag[x] != null) {
-                price += bag[x].getPrice();
-            }
+        for (int x = 0; x < size; x++) {
+        	salesPrice += bag[x].getPrice();
         }
-        return price;
-
+        
+        return Math.floor(salesPrice * 100) / 100;
     }
 
     /**
-     *
-     * @return taxprice
+     * Calulates the sales tax of items in the shopping bag
+     * 
+     * @return total sales tax
      */
     public double salesTax() {
-        double taxprice = 0;
+    	double taxRate = 0.06625;
+        double salesTax = 0;
 
-        for (int t = 0; t < bag.length; t++) {
-            if (bag[t] != null && bag[t].getTax() == true) {
-                taxprice += bag[t].getPrice() * 0.06625;
-
-            }
-
+        for (int i = 0; i < this.size; i++) {
+        	if (bag[i].getTax()) {
+        		salesTax += bag[i].getPrice() * taxRate;
+        	}
         }
-
-        return taxprice;
-
+        
+        return Math.floor(salesTax * 100) / 100;
     }
 
     /**
-     *
-     * @return salesPrice() + salesTax()
+     * print all items in bag
      */
-    public double totalPrice() {
-        return salesPrice() + salesTax();
-    }
-
-    //print all items in bag
     public void print() {
-
-        //TITLE 
-        System.out.println("**you have " + getSize() + " items in bag:"); ///title statment
-
-        //to print all the items
-        for (int e = 0; e < bag.length; e++) {
-            if (bag[e] != null) {
-
-                System.out.println(bag[e].toString());
-
-            }
+        for (int e = 0; e < size; e++) { // print all the items
+            System.out.println("·" + bag[e].toString());
         }
-
+    }
+    
+    /**
+     * Shows the number of items in the shopping bag
+     * 
+     * @return integer size of shopping bag
+     */
+    public int getSize() {
+        return this.size;
+    }
+    
+    /**
+     * Checks the size of the shopping bag for items
+     * 
+     * @return true if the shopping bag is empty
+     */
+    public boolean isEmpty() {
+    	if (size > 0) {
+    		return false;
+    	}
+    	return true;
+    }
+    
+    /**
+     * Nulls all items in the shopping bag array and resets size to 0
+     */
+    public void emptyBag() {
+        for (int i = 0; i < size; i++) {
+        	bag[i] = null;
+        }
+        size = 0;
     }
 
-    public GroceryItem[] getBag() {
-        return bag;
+    /**
+     * Doubles the max size of the shopping bag
+     */
+    private void grow() {
+        GroceryItem[] newBag = new GroceryItem[bag.length * 2];
+        
+        for (int i = 0; i < size; i++) {
+        	newBag[i] = bag[i];
+        }
+        bag = newBag;
     }
-
+    
     /**
      * helper method to find an item
      *
@@ -199,30 +146,87 @@ public class ShoppingBag {
      * @return index
      */
     private int find(GroceryItem item) {
-
-        int index = -1;
-
-        for (int x = 0; x < bag.length; x++) {
-
-            if (item.equals(bag[x]) && bag[x] != null) {
-
-                index = x; //x means item found at a particular index
-
-                break;
-            } else if (bag[x] == null || !item.equals(bag[x])) {
-
-                index = -1; //0 means item not found
-
-            }
-
+        for (int i = 0; i < size; i++) {
+        	if (bag[i].equals(item)) {
+        		return i;
+        	}
         }
-
-        return index;
-
+        
+        return -1;
     }
 
     public static void main(String[] args) {
-
+    	ShoppingBag testBag = new ShoppingBag();
+    	GroceryItem test1 = new GroceryItem("I", 5.99, false);
+    	GroceryItem test2 = new GroceryItem("like", 4.50, false);
+    	GroceryItem test3 = new GroceryItem("sushi_and_burger", 1.5, false);
+    	GroceryItem test4 = new GroceryItem("hello", 40, true);
+    	GroceryItem test5 = new GroceryItem("world", 60, true);
+    	
+    	// test getSize() 1
+    	System.out.println("Size zero: " + testBag.getSize() + "\n");
+    	
+    	// test remove() 1
+    	System.out.println("Invalid remove: " + testBag.remove(test1) + "\n");
+    	
+    	// test add() and print()
+    	System.out.println("Testing add:");
+    	testBag.add(test1);
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test remove() 2
+    	System.out.println("Valid remove: " + testBag.remove(test1));
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test grow() 1
+    	testBag.add(test1);
+    	testBag.add(test2);
+    	testBag.add(test3);
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test getSize() 2
+    	System.out.println("Size three: " + testBag.getSize() + "\n");
+    	
+    	// test grow() 2
+    	testBag.add(test1);
+    	testBag.add(test2);
+    	testBag.add(test3);
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test isEmpty() 1
+    	System.out.println("Bag is empty: " + testBag.isEmpty() + "\n");
+    	
+    	// test emptyBag()
+    	System.out.println("Emptying bag: ");
+    	testBag.emptyBag();
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test isEmpty() 2
+    	System.out.println("Bag is empty: " + testBag.isEmpty() + "\n");
+    	
+    	// test print() 2
+    	testBag.add(test4);
+    	testBag.add(test5);
+    	testBag.print();
+    	System.out.println();
+    	
+    	// test salesPrice() 1
+    	System.out.println("Sales Price 100: $" + testBag.salesPrice() + "\n");
+    	
+    	// test salesTax() 1
+    	System.out.println("Sales Tax 6.62: $" + testBag.salesTax() + "\n");
+    	
+    	// test remove() 3
+    	testBag.add(test1);
+    	testBag.add(test1);
+    	testBag.add(test1);
+    	System.out.println("Duplicate remove: " + testBag.remove(test1));
+    	testBag.print();
+    	System.out.println();
     }
-
 }

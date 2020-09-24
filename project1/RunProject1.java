7a2c1d4de041cbd9d77a8bead7089dce3aa78497
98changl <98changl@gmail.com>
2020-09-24 15:51:08 -0400

    Update RunProject1.java

--- project1/RunProject1.java
+++ project1/RunProject1.java
@@ -1,13 +1,10 @@
 /**
  * This is a driver class to run project 1
- * @author Liman Chang, Kenneth Christian
+ * @author Kenneth Christian, Liman Chang
  */
 
 public class RunProject1 {
-
 	public static void main(String[] args) {
 		new Shopping().run();
-
 	}
-
 }

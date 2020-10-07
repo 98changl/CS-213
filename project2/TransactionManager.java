f3a410dead28550ea33d957f066a40d4329dff65
98changl <98changl@gmail.com>
2020-10-07 19:49:32 -0400

    Add files via upload

--- project2/TransactionManager.java
+++ project2/TransactionManager.java
@@ -10,6 +10,66 @@ import java.util.Scanner;
 public class TransactionManager {
 	
 	/**
+	 * Print statements for the result of opening an account.
+	 * @param result of the account database call
+	 */
+	private void printOpen(boolean result) {
+		if (result == true) {
+			System.out.println("Account opened and added to the database.");
+		} else {
+			System.out.println("Account is already in the database.");
+		}
+	}
+	
+	/**
+	 * Print statements for the result of closing an account.
+	 * @param result of the account database call
+	 */
+	private void printClose(boolean result) {
+		if (result == true) {
+			System.out.println("Account closed and removed from the database.");
+		} else {
+			System.out.println("Account does not exist.");
+		}
+	}
+
+	/**
+	 * Print statements for the result of depositing money from an account.
+	 * @param result of the account database call
+	 * @param amount of money to deposit
+	 */
+	private void printDeposit(boolean result, double amount) {
+		DecimalFormat currency = new DecimalFormat("0,000.00");
+		
+		if (result == true) {
+			System.out.println(currency.format(amount) + " deposited to account.");
+		} else {
+			System.out.println("Account does not exist.");
+		}
+	}
+	
+	/**
+	 * Print statements for the result of withdrawing money from an account.
+	 * @param result of the account database call
+	 * @param amount of money to withdraw
+	 */
+	private void printWithdraw(int result, double amount) {
+		DecimalFormat currency = new DecimalFormat("0,000.00");
+		
+		if (result == 0) {
+			System.out.println(currency.format(amount) + " withdrawn from account.");
+		} 
+		else if (result == 1) {
+			System.out.println("Insufficient funds.");
+		} 
+		else if (result == -1) {
+			System.out.println("Account does not exist.");
+		} else {
+			System.out.println("Input data type mismatch.");
+		}
+	}
+	
+	/**
 	 * Converts a string representation of a date into a date object.
 	 * Does not perform exception handling.
 	 * @param date in the form of a string
@@ -21,7 +81,7 @@ public class TransactionManager {
 	}
 	
 	/**
-	 * Creates a Checking account.
+	 * Creates a Checking account given the string input.
 	 * @param input from the command line
 	 * @return Checking account with data from input, null on exception
 	 */
@@ -34,6 +94,11 @@ public class TransactionManager {
 		Date date = new Date(1, 1, 2000);
 		boolean directDeposit = false;
 		
+		System.out.println(elements.length);
+		if (elements.length == 3) { // C commands
+			return new Checking(first_name, last_name, balance, date, directDeposit);
+		}
+		
 		// set up balance
 		try {
 			balance = Double.parseDouble(elements[3]);
@@ -41,7 +106,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 3) { // close account
+		if (elements.length == 4) { // D & W commands
 			return new Checking(first_name, last_name, balance, date, directDeposit);
 		} 
 		
@@ -52,7 +117,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 4) { // deposit or withdraw funds
+		if (elements.length == 5) { // O commands
 			return new Checking(first_name, last_name, balance, date, directDeposit);
 		}
 		
@@ -67,7 +132,7 @@ public class TransactionManager {
 	}
 	
 	/**
-	 * Creates a Savings account.
+	 * Creates a Savings account given the string input.
 	 * @param input from the command line
 	 * @return Savings account with data from input, null on exception
 	 */
@@ -80,6 +145,10 @@ public class TransactionManager {
 		Date date = new Date(1, 1, 2000);
 		boolean isLoyal = false;
 		
+		if (elements.length == 3) { // C commands
+			return new Savings(first_name, last_name, balance, date, isLoyal);
+		}
+		
 		// set up balance
 		try {
 			balance = Double.parseDouble(elements[3]);
@@ -87,7 +156,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 3) { // close account
+		if (elements.length == 4) { // D & W commands
 			return new Savings(first_name, last_name, balance, date, isLoyal);
 		} 
 		
@@ -98,7 +167,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 4) { // deposit or withdraw funds
+		if (elements.length == 5) { // O commands
 			return new Savings(first_name, last_name, balance, date, isLoyal);
 		}
 		
@@ -113,7 +182,7 @@ public class TransactionManager {
 	}
 	
 	/**
-	 * Creates a MoneyMarket account.
+	 * Creates a MoneyMarket account given the string input.
 	 * @param input from the command line
 	 * @return MoneyMarket account with data from input, null on exception
 	 */
@@ -126,6 +195,10 @@ public class TransactionManager {
 		Date date = new Date(1, 1, 2000);
 		int withdrawals = 0;
 		
+		if (elements.length == 3) { // C commands
+			return new MoneyMarket(first_name, last_name, balance, date, withdrawals);
+		}
+		
 		// set up balance
 		try {
 			balance = Double.parseDouble(elements[3]);
@@ -133,7 +206,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 3) { // close account
+		if (elements.length == 4) { // D & W commands
 			return new MoneyMarket(first_name, last_name, balance, date, withdrawals);
 		} 
 		
@@ -144,7 +217,7 @@ public class TransactionManager {
 		catch (NumberFormatException e) {
 			return null;
 		}
-		if (elements.length == 4) { // deposit or withdraw funds
+		if (elements.length == 5) { // O commands
 			return new MoneyMarket(first_name, last_name, balance, date, withdrawals);
 		}
 		
@@ -161,7 +234,7 @@ public class TransactionManager {
 	/**
 	 * Takes the String representation of the command line input and creates a double of the amount stated in the input.
 	 * @param input from the command line
-	 * @return double of the amount stated from the input, -1 if exception occured
+	 * @return double of the amount stated from the input, -1 if exception occurred
 	 */
 	private double getAmount(String input) {
 		String[] elements = input.split(" ");
@@ -181,220 +254,193 @@ public class TransactionManager {
 	public void run() {
 		Scanner scn = new Scanner(System.in).useDelimiter("\\s+"); // to get input for command and data
 		AccountDatabase database = new AccountDatabase();
-		DecimalFormat currency = new DecimalFormat("0,000.00");
+		//DecimalFormat currency = new DecimalFormat("0,000.00");
 		String input = new String(); // string to store console inputs
 		boolean running = true; // boolean for command loop
 		
 		System.out.println("Transaction processing starts.....");
-		// command loop
-		while (running) {
+		while (running) { // command loop
 			String command;
 			boolean result = false;
+			int withdrawal_result = -2;
 			
 			try {
 				input = scn.nextLine();
 			}
 			catch (InputMismatchException e) {
-				System.out.println("InputMismatchException: ");
+				System.out.println("InputMismatchException");
 				continue;
 			}
 			
-			command = input.substring(0, 1);
+			if (input.length() > 1) {
+				command = input.substring(0, 2);
+			} else {
+				command = input.substring(0, 1);
+			}
+			
 			command = command.trim();
 			
-			// open
-			if (command.equals("O")) {
-				command = input.substring(0, 2);
-				
-				if (command.equals("OC")) {
-					Checking acc = createChecking(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.add(acc);
-					}
-				}
-				else if (command.equals("OS")) {
-					Savings acc = createSavings(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.add(acc);
-					}
-				} 
-				else if (command.equals("OM")) {
-					MoneyMarket acc = createMoneyMarket(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.add(acc);
-					}
-				}
-				
-				if (result == true) {
-					System.out.println("Account opened and added to the database.");
+			// open commands
+			if (command.equals("OC")) {
+				Checking acc = createChecking(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
 				} else {
-					System.out.println("Account is already in the database.");
+					result = database.add(acc);
 				}
+				printOpen(result);
+			}
+			else if (command.equals("OS")) {
+				Savings acc = createSavings(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.add(acc);
+				}
+				printOpen(result);
+			} 
+			else if (command.equals("OM")) {
+				MoneyMarket acc = createMoneyMarket(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.add(acc);
+				}
+				printOpen(result);
 			}
 			
-			// close
-			if (command.equals("C")) {
-				command = input.substring(0, 2);
-				
-				if (command.equals("CC")) {
-					Checking acc = createChecking(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.remove(acc);
-					}
-				}
-				else if (command.equals("CS")) {
-					Savings acc = createSavings(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.remove(acc);
-					}
-				} 
-				else if (command.equals("CM")) {
-					MoneyMarket acc = createMoneyMarket(input);
-					if (acc == null) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.remove(acc);
-					}
-				}
-				
-				if (result == true) {
-					System.out.println("Account closed and removed from the database.");
+			// close commands
+			else if (command.equals("CC")) {
+				Checking acc = createChecking(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
 				} else {
-					System.out.println("Account does not exist.");
+					result = database.remove(acc);
 				}
+				printClose(result);
+			}
+			else if (command.equals("CS")) {
+				Savings acc = createSavings(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.remove(acc);
+				}
+				printClose(result);
+			} 
+			else if (command.equals("CM")) {
+				MoneyMarket acc = createMoneyMarket(input);
+				if (acc == null) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.remove(acc);
+				}
+				printClose(result);
 			}
 			
-			// deposit
-			if (command.equals("D")) {
-				command = input.substring(0, 2);
-				
-				if (command.equals("DC")) {
-					Checking acc = createChecking(input);
-					double amount = getAmount(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.deposit(acc, amount);
-					}
-				}
-				else if (command.equals("DS")) {
-					Savings acc = createSavings(input);
-					double amount = getAmount(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.deposit(acc, amount);
-					}
-				} 
-				else if (command.equals("DM")) {
-					MoneyMarket acc = createMoneyMarket(input);
-					double amount = getAmount(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						result = database.deposit(acc, amount);
-					}
-				}
-				
-				if (result == true) {
-					System.out.println(currency.format(getAmount(input)) + " deposited to account.");
-				} else {
-					System.out.println("Account does not exist.");
-				}
-			}
-			
-			// withdraw
-			if (command.equals("W")) {
-				command = input.substring(0, 2);
-				int withdrawal_result = -2;
+			// deposit commands
+			else if (command.equals("DC")) {
+				Checking acc = createChecking(input);
 				double amount = getAmount(input);
 				
-				if (command.equals("WC")) {
-					Checking acc = createChecking(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						withdrawal_result = database.withdrawal(acc, amount);
-					}
-				}
-				else if (command.equals("WS")) {
-					Savings acc = createSavings(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						withdrawal_result = database.withdrawal(acc, amount);
-					}
-				} 
-				else if (command.equals("WM")) {
-					MoneyMarket acc = createMoneyMarket(input);
-					
-					if (acc == null || amount == -1) {
-						System.out.println("Input data type mismatch.");
-						continue;
-					} else {
-						withdrawal_result = database.withdrawal(acc, amount);
-					}
-				}
-				
-				if (withdrawal_result == 0) {
-					System.out.println(currency.format(amount) + " withdrawn from account.");
-				} 
-				else if (withdrawal_result == 1) {
-					System.out.println("Insufficient funds.");
-				} 
-				else if (withdrawal_result == -1) {
-					System.out.println("Account does not exist.");
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
 				} else {
-					
+					result = database.deposit(acc, amount);
 				}
+				printDeposit(result, amount);
+			}
+			else if (command.equals("DS")) {
+				Savings acc = createSavings(input);
+				double amount = getAmount(input);
+				
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.deposit(acc, amount);
+				}
+				printDeposit(result, amount);
+			} 
+			else if (command.equals("DM")) {
+				MoneyMarket acc = createMoneyMarket(input);
+				double amount = getAmount(input);
+				
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					result = database.deposit(acc, amount);
+				}
+				printDeposit(result, amount);
 			}
 			
-			// print
-			if (command.equals("PA")) {
+			// withdrawal commands
+			else if (command.equals("WC")) {
+				Checking acc = createChecking(input);
+				double amount = getAmount(input);
+				
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					withdrawal_result = database.withdrawal(acc, amount);
+				}
+				printWithdraw(withdrawal_result, amount);
+			}
+			else if (command.equals("WS")) {
+				Savings acc = createSavings(input);
+				double amount = getAmount(input);
+				
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					withdrawal_result = database.withdrawal(acc, amount);
+				}
+				printWithdraw(withdrawal_result, amount);
+			} 
+			else if (command.equals("WM")) {
+				MoneyMarket acc = createMoneyMarket(input);
+				double amount = getAmount(input);
+				
+				if (acc == null || amount == -1) {
+					System.out.println("Input data type mismatch.");
+					continue;
+				} else {
+					withdrawal_result = database.withdrawal(acc, amount);
+				}
+				printWithdraw(withdrawal_result, amount);
+			}
+			
+			// print commands
+			else if (command.contains("PA")) {
 				database.printByDateOpen();
 			}
-			else if (command.equals("PD")) {
+			else if (command.contains("PD")) {
 				database.printAccounts();
 			} 
-			else if (command.equals("PN")) {
+			else if (command.contains("PN")) {
 				database.printByLastName();
 			}
-			
-			// quit
-			if (command.equals("Q")) {
+			else if (command.equals("Q")) { // quit
 				System.out.println("Transaction processing completed.");
 				running = false;
 			} else {
-				command = input.substring(0, 2);
+				if (input.length() > 1) {
+					command = input.substring(0, 2);
+				}
 				command.trim();
 				System.out.println("Command '" + command + "' not supported!");
 			}
 		}
+		scn.close();
 	}
 }

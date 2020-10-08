f58363fd36bb559c944f2fb2479094b491f946d3
kennethchris99 <71343141+kennethchris99@users.noreply.github.com>
2020-10-08 13:55:22 -0400

    Update Checking.java

--- project2/Checking.java
+++ project2/Checking.java
@@ -3,26 +3,43 @@ package project2;
 import java.text.DecimalFormat;
 
 /**
- * 
+ * This is a sub class of Account and creates a Account of type Checking.
  * @author Liman Chang, Kenneth Christian
  */
 public class Checking extends Account {
 	private boolean directDeposit;
 	
+        /**
+         * Constructor for Checking
+         * @param first_name
+         * @param last_name
+         * @param balance
+         * @param date
+         * @param directDeposit 
+         */
 	public Checking(String first_name, String last_name, double balance, Date date, boolean directDeposit) {
 		super(first_name, last_name, balance, date);
 		this.directDeposit = directDeposit;
 	}
-	
+
+    
+	/**
+         * 
+         * @return value of monthly interest
+         */
 	@Override
 	public double monthlyInterest() {
     
-                      
-            //0.0005/12
+           //0.0005/12
             return 0.00004 ;
 
 	}
        
+        /**
+         * 
+         * @param balance
+         * @return monthly fee of 0 or 25.  
+         */
 	@Override
 	public double monthlyFee(double balance) {
             if(balance >= 1500 || directDeposit)
@@ -41,4 +58,5 @@ public class Checking extends Account {
      public boolean isDd(){
          return this.directDeposit;
      }
+    
 }

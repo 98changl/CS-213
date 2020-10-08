424062640638fe81278ebe509e34870737546da0
kennethchris99 <71343141+kennethchris99@users.noreply.github.com>
2020-10-08 13:56:03 -0400

    Update Savings.java

--- project2/Savings.java
+++ project2/Savings.java
@@ -1,17 +1,29 @@
 package project2;
 
 /**
- * 
+ * This is a sub class of Account and creates a Account of type Savings.
  * @author Liman Chang, Kenneth Christian
  */
 public class Savings extends Account {
 	private boolean isLoyal;
 
+        /**
+         * Constructor for Savings
+         * @param first_name
+         * @param last_name
+         * @param balance
+         * @param date
+         * @param isLoyal 
+         */
 	public Savings(String first_name, String last_name, double balance, Date date, boolean isLoyal) {
 		super(first_name, last_name, balance, date);
 		this.isLoyal = isLoyal;
 	}
 	
+        /**
+         * Method returns monthly interest. Value depends on the account details.
+         * @return monthly interest. 
+         */
 	@Override
 	public double monthlyInterest() {
                 if(isLoyal){
@@ -23,7 +35,12 @@ public class Savings extends Account {
                 
             
 	}
-
+        
+        /**
+         * 
+         * @param balance
+         * @return 0 or 5 depending on the details of the account 
+         */
 	@Override
 	public double monthlyFee(double balance) {
                 if(balance >= 300){

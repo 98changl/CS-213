e2d9b6cd6ff2067f4e66868a8d9111e0ff596335
98changl <98changl@gmail.com>
2020-11-14 21:25:57 -0500

    Updated methods for Model classes

---
+++ Extra.java
@@ -0,0 +1,13 @@
+package project4;
+
+public class Extra {
+	String ingredient = "";
+		
+	public Extra(String ingredient) {
+		this.ingredient = ingredient;
+	}
+	
+	public String toString() {
+		return this.ingredient;
+	}
+}

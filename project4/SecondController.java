751c7181a4435a76c8793a0c646468ba70f40e96
kennethchris99 <71343141+kennethchris99@users.noreply.github.com>
2020-11-15 17:13:26 -0500

    Update SecondController.java

--- project4/SecondController.java
+++ project4/SecondController.java
@@ -1,5 +1,42 @@
 package project4;
 
-public class SecondController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class SecondController implements Initializable {
+
+
+    @FXML
+    public ListView DetailsListView;
+
+    @FXML
+    public Button AddSelectedSandwichB , RemoveSandwichB, ClearSandwichB ,SaveB;
+
+    public Controller controller1;
+
+
+
+
+
+
+    /**
+     * Called when view loads.
+     *
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+    }
 }

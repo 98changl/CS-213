cb49eadeaaba1ed6687abc528cde2d055693986f
kennethchris99 <71343141+kennethchris99@users.noreply.github.com>
2020-12-06 13:17:54 -0500

    Update MainActivity.java

--- project5/MainActivity.java
+++ project5/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.project5;
 
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,10 +20,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-   // public Button MoMA_PS1 ,New_York_Historical_Society ,Rubin_Museum, Whitney_Museum_of_American_Art ;
-
-
-
     /**
      * Method to perform application start up.
      *
@@ -31,18 +30,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final Button MoMA = findViewById(R.id.MoMA_PS1);
         final Button NYHS = findViewById(R.id.New_York_Historical_Society);
         final Button RM = findViewById(R.id.Rubin_Museum);
         final Button WMOAA = findViewById(R.id.Whitney_Museum_of_American_Art);
 
-
-        //MOMA = 1 , NYHS = 2 , RM = 3, WMOAA = 4
+       //Moma PS1 button click event
         MoMA.setOnClickListener(new View.OnClickListener() {
 
+            /**
+             * Method to handle button click for MoMA PS1
+             * and start activity 2 to calculate price and
+             * give access to MoMA PS1 website.
+             *
+             * @param view
+             */
             public void onClick(View view) {
 
-                //student, adult, senior
+                //student, adult, senior, 1 == MoMA PS1
                 int price[] = {14 , 25 , 18 , 1};
                 Intent intent = new Intent(MainActivity.this , Activity2.class);
                 intent.putExtra("price" ,price);
@@ -51,10 +57,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //New-York Historical Society button click event
         NYHS.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Method to handle button click for New-York Historical Society
+             * and start activity 2 to calculate price and
+             * give access to New-York Historical Society website.
+             *
+             * @param v
+             */
             public void onClick(View v) {
 
-                //student, adult, senior
+                //student, adult, senior 2 == New-York Historical Society
                 int price[] = {13 , 22 , 17 , 2};
                 Intent intent = new Intent(MainActivity.this , Activity2.class);
                 intent.putExtra("price" , price);
@@ -63,10 +78,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Rubin Museum button click event
         RM.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Method to handle button click for Rubin Museum
+             * and start activity 2 to calculate price and
+             * give access to Rubin Museum website.
+             *
+             * @param v
+             */
             public void onClick(View v) {
 
-                //student, adult, senior
+                //student, adult, senior, 3 == Rubin Museum
                 int price[] = {14 , 19 , 14 , 3};
                 Intent intent = new Intent(MainActivity.this , Activity2.class);
                 intent.putExtra("price" , price);
@@ -75,10 +99,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Whitney Museum of American Art button click event
         WMOAA.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Method to handle button click for Whitney Museum of American Art
+             * and start activity 2 to calculate price and
+             * give access to Whitney Museum of American Art website.
+             *
+             * @param v
+             */
             public void onClick(View v) {
 
-                //student, adult, senior
+                //student, adult, senior, 4 == Whitney Museum of American Art
                 int price[] = {18 , 25 , 18 , 4};
                 Intent intent = new Intent(MainActivity.this , Activity2.class);
                 intent.putExtra("price" , price);
@@ -90,6 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    
+
 
 }
